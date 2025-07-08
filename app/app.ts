438e3ffe012efc3712d8bf869e9d31e72436c504
Eugene Danilovets <eugene.danilovets@gmail.com>
2025-07-08 17:57:03 +0300

    feat: instantiate DashboardPage in App class

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,8 @@
 import { BaseComponent } from './base.component';
+import { DashboardPage } from './pages/dashboard.page';
 import { LoginPage } from './pages/login.page';
 
 export class App extends BaseComponent {
   loginPage = new LoginPage(this.page);
+  dashboardPage = new DashboardPage(this.page);
 }
